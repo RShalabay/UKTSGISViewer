5627e8f7355488202f431a9b203d0e2da1a96eab
romansalabaj <mc_romcheg@mail.ru>
2021-12-22 14:19:57 +0600

    444

--- app/src/main/java/kz/ukteplo/uktsgisviewer/MapsActivity.kt
+++ app/src/main/java/kz/ukteplo/uktsgisviewer/MapsActivity.kt
@@ -11,6 +11,12 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.clustering.ClusterItem
+import com.google.maps.android.clustering.ClusterManager
+import com.google.maps.android.collections.GroundOverlayManager
+import com.google.maps.android.collections.MarkerManager
+import com.google.maps.android.collections.PolygonManager
+import com.google.maps.android.collections.PolylineManager
 import com.google.maps.android.data.kml.KmlLayer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -60,90 +66,42 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, CoroutineScope {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        //mMap.uiSettings.isMyLocationButtonEnabled = true
 
-
-
-        /*try {
-
-        } catch (e: Exception) {
-            Log.e("MAP", e.localizedMessage)
-            e.printStackTrace()
-        }*/
-        //}
-// Add a marker in Sydney and move the camera
-        val sydney = LatLng(50.369566, 83.311246)
+        /*val sydney = LatLng(50.369566, 83.311246)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
-        job = Job()
-
-        try{
-            launch(Dispatchers.IO) {
-                layer = KmlLayer(mMap, R.raw.streets, applicationContext)
-                launch(Dispatchers.Main) {
-                    layer.addLayerToMap()
-                    Toast.makeText(applicationContext, "Слой Улицы загружен", Toast.LENGTH_SHORT)
-                }
-                Log.d("MAP", "Слой Улицы загружен")
-            }
-           launch(Dispatchers.IO) {
-                layer1 = KmlLayer(mMap, R.raw.house, applicationContext)
-                launch(Dispatchers.Main) {
-                    layer1.addLayerToMap()
-                    Toast.makeText(applicationContext, "Слой Жилые загружен", Toast.LENGTH_SHORT)
-                }
-                Log.d("MAP", "Слой Жилые загружен")
-            }
-
-            launch(Dispatchers.IO) {
-                layer2 = KmlLayer(mMap, R.raw.irtysh, applicationContext)
-                launch(Dispatchers.Main) {
-                    layer2.addLayerToMap()
-                    Toast.makeText(applicationContext, "Слой Иртыш загружен", Toast.LENGTH_SHORT)
-                }
-                Log.d("MAP", "Слой Иртыш загружен")
-            }
-
-            launch(Dispatchers.IO) {
-                layer3 = KmlLayer(mMap, R.raw.labels, applicationContext)
-                launch(Dispatchers.Main) {
-                    layer3.addLayerToMap()
-                    Toast.makeText(applicationContext, "Слой Метки загружен", Toast.LENGTH_SHORT)
-                }
-                Log.d("MAP", "Слой Метки загружен")
-            }
-
-            launch(Dispatchers.IO) {
-                layer4 = KmlLayer(mMap, R.raw.net, applicationContext)
-                launch(Dispatchers.Main) {
-                    layer4.addLayerToMap()
-                    Toast.makeText(applicationContext, "Слой Сети загружен", Toast.LENGTH_SHORT)
-                }
-                Log.d("MAP", "Слой Сети загружен")
-            }
-        }catch (e: Exception) {
-            Log.e("MAP", e.localizedMessage)
-        }
-
-
-
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))*/
+        drawKml(mMap)
         //job = Job()
-        //launch {
-        /*    try {
-                layer = KmlLayer(mMap, R.raw.streets, applicationContext)
-                layer.addLayerToMap()
-                layer1 = KmlLayer(mMap, R.raw.house, applicationContext)
-                layer1.addLayerToMap()
-                layer2 = KmlLayer(mMap, R.raw.irtysh, applicationContext)
-                layer2.addLayerToMap()
-                layer3 = KmlLayer(mMap, R.raw.labels, applicationContext)
-                layer3.addLayerToMap()
-                layer4 = KmlLayer(mMap, R.raw.net, applicationContext)
-                layer4.addLayerToMap()
-            } catch (e: Exception) {
-                Log.e("MAP", e.localizedMessage)
-                e.printStackTrace()
-            }*/
-        //}
+
+
+    }
+
+    fun drawKml(map: GoogleMap) {
+        val markerManager = MarkerManager(map)
+        val groundOverlayManager = GroundOverlayManager(map!!)
+        val polygonManager = PolygonManager(map)
+        val polylineManager = PolylineManager(map)
+
+        val clusterManager = ClusterManager<ClusterItem>(applicationContext, map, markerManager)
+
+        val kml_streets = KmlLayer(map,
+            R.raw.streets,
+            applicationContext,
+            markerManager,
+            polygonManager,
+            polylineManager,
+            groundOverlayManager,
+            null)
+            .addLayerToMap()
+        val kml_irtysh = KmlLayer(map,
+            R.raw.irtysh,
+            applicationContext,
+            markerManager,
+            polygonManager,
+            polylineManager,
+            groundOverlayManager,
+            null)
+            .addLayerToMap()
     }
 }
